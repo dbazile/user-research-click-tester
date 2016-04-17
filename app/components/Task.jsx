0f8a11d0846cb44dcc34c64c6a023c2208c3e2d3
David Bazile <davidbazile@gmail.com>
2016-04-16 20:25:15 -0400

    Move normalization to call site

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -58,17 +58,12 @@ export default class Task extends Component {
     const {canvas} = this.refs;
     const context = canvas.getContext('2d');
 
-    // Normalize
-    const normalizedX = Math.floor(x);
-    const normalizedY = Math.floor(y);
-    const normalizedRadius = Math.floor(radius);
-
     // Inside
     context.fillStyle = 'hsla(335, 75%, 50%, .3)';
     context.lineWidth = Math.ceil(radius * .12);
     context.strokeStyle = 'hsla(335, 75%, 50%, .7)';
     context.beginPath();
-    context.arc(normalizedX, normalizedY, normalizedRadius, 0, 2 * Math.PI);
+    context.arc(x, y, radius, 0, 2 * Math.PI);
     context.closePath();
     context.fill();
     context.stroke();
@@ -77,7 +72,7 @@ export default class Task extends Component {
     context.lineWidth = Math.ceil(radius * .18);
     context.strokeStyle = 'lime';
     context.beginPath();
-    context.arc(normalizedX, normalizedY, Math.ceil(normalizedRadius * 1.4), 0, 2 * Math.PI);
+    context.arc(x, y, Math.ceil(radius * 1.4), 0, 2 * Math.PI);
     context.closePath();
     context.stroke();
   }
@@ -89,9 +84,9 @@ export default class Task extends Component {
   _onClick(event) {
     const {canvas} = this.refs;
     const aspectRatio = (canvas.width / canvas.offsetWidth);
-    const scaledX = event.layerX * aspectRatio;
-    const scaledY = event.layerY * aspectRatio;
-    const scaledRadius = MARKER_RADIUS * (aspectRatio / 2);
+    const scaledX = Math.floor(event.layerX * aspectRatio);
+    const scaledY = Math.floor(event.layerY * aspectRatio);
+    const scaledRadius = Math.floor(MARKER_RADIUS * (aspectRatio / 2));
     this._drawMarker(scaledX, scaledY, scaledRadius);
     console.debug('click event', event);
     // this.props.onClick(event.offsetX * /  canvas.offsetWidth, event.offsetY / canvas);
