32165159252d105141b817116f2bb50ff11a18bc
David Bazile <davidbazile@gmail.com>
2016-04-17 17:04:44 -0400

    Add "thank you" block

--- app/components/ThankYou.jsx
+++ app/components/ThankYou.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
+import ThankYouMural from './ThankYouMural';
 import styles from './ThankYou.css';
 
 export default class ThankYou extends Component {
   render() {
     return (
-      <div className={styles.root}>
-        <h1>ThankYou</h1>
-      </div>
+      <section className={styles.root}>
+        <div className={styles.column}>
+          <ThankYouMural className={styles.mural}/>
+        </div>
+        <div className={styles.column}>
+          <h1 className={styles.heading}>Thank You!</h1>
+          <p>I quite literally could not have done this without your help.</p>
+          <p>...Uhh, could you please let me know that you're done? :)</p>
+          <button className={styles.transmit}>Click here to transmit results</button>
+        </div>
+      </section>
     );
   }
 }
